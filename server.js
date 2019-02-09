246fcdb29cfa7250fa2aa061487b6095f80255f9
Packia Sheela Varadhan <packiasheelav@gmail.com>
2019-02-10 00:28:25 +0200

    first commit

--- server.js
+++ server.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next)=>{
-  res.send('hello world');
-})
+app.use('/graphql', graphqlHttp({
+  graphqlHttp({
+    schema: buildSchema(`
+    type Event {
+      _id: ID!
+      description:String!
+      price: Float!
+      date:Sting!
+    }
+    type RootQuery {
+      events: [Event!]!
+    }
+    type RootMutation {
+      createEvent(name : String): Event
+    }
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+    `),
+rootValue: {
+  events: () => {
+     return ['Studying', 'Teaching', 'Swimming']
+  },
+  
+},
+createEvents: (args) => {
+  const eventName = args.name;
+  return eventName;
+},
+graphiql : true
+
+  })
+}));
 
 
 app.listen(3000);
