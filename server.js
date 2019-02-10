fc78df5678d2de7baab719e0c1cc00ab1424e483
Packia Sheela Varadhan <packiasheelav@gmail.com>
2019-02-10 23:23:45 +0200

    first commit

--- server.js
+++ server.js
@@ -1,45 +1,87 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+
+const Event = require("./models/event");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp({
+app.use(
+  "/graphql",
   graphqlHttp({
     schema: buildSchema(`
-    type Event {
-      _id: ID!
-      description:String!
-      price: Float!
-      date:Sting!
-    }
-    type RootQuery {
-      events: [Event!]!
-    }
-    type RootMutation {
-      createEvent(name : String): Event
-    }
-    schema {
-      query: RootQuery
-      mutation: RootMutation
-    }
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+        type RootQuery {
+            events: [Event!]!
+        }
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
     `),
-rootValue: {
-  events: () => {
-     return ['Studying', 'Teaching', 'Swimming']
-  },
-  
-},
-createEvents: (args) => {
-  const eventName = args.name;
-  return eventName;
-},
-graphiql : true
-
+    rootValue: {
+      events: () => {
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      }
+    },
+    graphiql: true
   })
-}));
+);
 
-
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@cluster0-4yj77.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
